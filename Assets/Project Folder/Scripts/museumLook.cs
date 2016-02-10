372cd0f55ede7dd495d38425e49ef590da6b702b
christian gonzalez <chrisangoro@gmail.com>
2016-02-10 18:07:43 -0500

    Commit 6
    A LOT OF MAYOR CHANGES
    
    - Completed the early "museum" stage
    - Started the movement and the invention's rotation script
    - Fixed some problems
    - Changed some prefabs to meet the rotation requirements

--- Assets/Project Folder/Scripts/museumLook.cs
+++ Assets/Project Folder/Scripts/museumLook.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class museumLook : MonoBehaviour {
 
 	[SerializeField] private Transform invento1;
-	//[SerializeField] private Transform invento2;
-	//[SerializeField] private Transform invento3;
-	//[SerializeField] private Transform invento4;
+	[SerializeField] private Transform invento2;
+	[SerializeField] private Transform invento3;
+	[SerializeField] private Transform invento4;
 	[SerializeField] private Ray cameraCenter;
 	[SerializeField] private Transform mainCamera;
 	[SerializeField] private TextMesh textI1;
@@ -20,24 +20,24 @@ public class museumLook : MonoBehaviour {
 	private float xDeg;
 	private float yDeg;
 	private Vector3 startPosInv1;
-	//private Vector3 startPosInv2;
-	//private Vector3 startPosInv3;
-	//private Vector3 startPosInv4;
+	private Vector3 startPosInv2;
+	private Vector3 startPosInv3;
+	private Vector3 startPosInv4;
 	private Quaternion startRotInv1;
 	//private Quaternion startRotInv2;
 	//private Quaternion startRotInv3;
 	//private Quaternion startRotInv4;
 	private bool lookedAtInv1;
-	//private bool lookedAtInv2;
-	//private bool lookedAtInv3;
-	//private bool lookedAtInv4;
+	private bool lookedAtInv2;
+	private bool lookedAtInv3;
+	private bool lookedAtInv4;
 
 	// Use this for initialization
 	void Start () {
 		startPosInv1 = invento1.position;
-		//startPosInv2 = invento2.position;
-		//startPosInv3 = invento3.position;
-		//startPosInv4 = invento4.position;
+		startPosInv2 = invento2.position;
+		startPosInv3 = invento3.position;
+		startPosInv4 = invento4.position;
 
 		startRotInv1 = invento1.rotation;
 		//startRotInv2 = invento2.rotation;
@@ -45,9 +45,9 @@ public class museumLook : MonoBehaviour {
 		//startRotInv4 = invento4.rotation;
 
 		lookedAtInv1 = false;
-		//lookedAtInv2 = false;
-		//lookedAtInv3 = false;
-		//lookedAtInv4 = false;
+		lookedAtInv2 = false;
+		lookedAtInv3 = false;
+		lookedAtInv4 = false;
 	}
 
 	// Update is called once per frame
@@ -55,15 +55,15 @@ public class museumLook : MonoBehaviour {
 
 		RaycastHit hit;
 
-		if(!lookedAtInv1){
+		if(!lookedAtInv1 && !lookedAtInv2 && !lookedAtInv3 && !lookedAtInv4){
 			var xMov = Input.GetAxis ("Horizontal");
 			var yMov = Input.GetAxis ("Vertical");
 
 			if (Mathf.Abs (xMov) > 0.01) {
-				mainCamera.parent.Translate (mainCamera.forward * xMov * movementSpeed);
+				mainCamera.parent.Translate (mainCamera.forward.normalized * xMov * movementSpeed);
 			}
 			if (Mathf.Abs (yMov) > 0.01) {
-				mainCamera.parent.Translate (mainCamera.right * yMov *movementSpeed);
+				mainCamera.parent.Translate (mainCamera.right.normalized * yMov *movementSpeed);
 			}
 		}
 
@@ -74,31 +74,121 @@ public class museumLook : MonoBehaviour {
 
 		if(Physics.Raycast(mainCamera.position, mainCamera.forward.normalized, out hit)){
 			Transform objectHit = hit.transform;
-			print ("mira a " + objectHit);													//test to see what the camera is looking
-			if (objectHit.name.Equals ("Davinci")) {
-				lookedAtInv1 = true;
-			}
+			print ("mira a " + objectHit);                                              //test to see what the camera is looking
 
-			if(lookedAtInv1){
+
+            if (objectHit.name.Equals ("Davinci")) {
+                if (Input.GetKey("q"))
+                {
+                    lookedAtInv1 = true;
+                }
+            }
+
+            if (objectHit.name.Equals("Bici")) {
+                if (Input.GetKey("q"))
+                {
+                    lookedAtInv2 = true;
+                }
+            }
+
+            if (objectHit.name.Equals("Intento")) {
+                if (Input.GetKey("q"))
+                {
+                    lookedAtInv3 = true;
+                }
+            }
+
+            if (objectHit.name.Equals("DaVinciCannon")) {
+                if (Input.GetKey("q"))
+                {
+                    lookedAtInv4 = true;
+                }
+            }
+
+/*---------------------------------------------------------INVENTO 1----------------------------------------------------------*/
+
+            if (lookedAtInv1){
 				print ("aleluya " + objectHit);
 				xDeg = (Input.GetAxis ("Horizontal"));
 				yDeg = (Input.GetAxis ("Vertical"));				
 
 				if (Mathf.Abs (xDeg) > 0.01) {
 					invento1.Rotate (0 , -1 * xDeg * rotVelX, 0);
-					//invento1.RotateAround (Vector3.up, Vector3.right, rotVelX);
-					//invento1.rotation = Quaternion.AngleAxis(rotVelX, invento1.up);
 				}
 				invento1.position = startPosInv1;
-				if (Mathf.Abs(yDeg) > 0.01 ) {
-					//invento1.rotation = Quaternion.AngleAxis(rotVelY, invento1.right);
-					//invento1.Rotate (0, xDeg*rotVelX*-1, 0);
+				/*if (Mathf.Abs(yDeg) > 0.01 ) {
 					invento1.Rotate (yDeg * rotVelY, 0, 0);
-				}
+				}*/
 				if (Input.GetKey ("q")) {
 					lookedAtInv1 = false;
 				}
 			}
-		}
+
+/*---------------------------------------------------------INVENTO 2----------------------------------------------------------*/
+
+            if (lookedAtInv2)
+            {
+                print("aleluya " + objectHit);
+                xDeg = (Input.GetAxis("Horizontal"));
+                yDeg = (Input.GetAxis("Vertical"));
+
+                if (Mathf.Abs(xDeg) > 0.01)
+                {
+                    invento2.Rotate(0, 0, -1 * xDeg * rotVelX);
+                }
+                invento2.position = startPosInv2;
+                /*if (Mathf.Abs(yDeg) > 0.01 ) {
+					invento1.Rotate (yDeg * rotVelY, 0, 0);
+				}*/
+                if (Input.GetKey("q"))
+                {
+                    lookedAtInv2 = false;
+                }
+            }
+
+/*---------------------------------------------------------INVENTO 3----------------------------------------------------------*/
+
+            if (lookedAtInv3)
+            {
+                print("aleluya " + objectHit);
+                xDeg = (Input.GetAxis("Horizontal"));
+                yDeg = (Input.GetAxis("Vertical"));
+
+                if (Mathf.Abs(xDeg) > 0.01)
+                {
+                    invento3.Rotate(0, 0, -1 * xDeg * rotVelX);
+                }
+                invento3.position = startPosInv3;
+                /*if (Mathf.Abs(yDeg) > 0.01 ) {
+					invento1.Rotate (yDeg * rotVelY, 0, 0);
+				}*/
+                if (Input.GetKey("q"))
+                {
+                    lookedAtInv3 = false;
+                }
+            }
+
+/*---------------------------------------------------------INVENTO 4----------------------------------------------------------*/
+
+            if (lookedAtInv4)
+            {
+                print("aleluya " + objectHit);
+                xDeg = (Input.GetAxis("Horizontal"));
+                yDeg = (Input.GetAxis("Vertical"));
+
+                if (Mathf.Abs(xDeg) > 0.01)
+                {
+                    invento4.Rotate(0, 0, -1 * xDeg * rotVelX);
+                }
+                invento4.position = startPosInv4;
+                /*if (Mathf.Abs(yDeg) > 0.01 ) {
+					invento1.Rotate (yDeg * rotVelY, 0, 0);
+				}*/
+                if (Input.GetKey("q"))
+                {
+                    lookedAtInv4 = false;
+                }
+            }
+        }
 	}
 }
